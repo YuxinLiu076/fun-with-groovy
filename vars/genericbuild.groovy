75a2010b4ebd3d033ede44d0dc64f7e4ce7b5a39
yuxinliu <lyx1141491623@gmail.com>
2023-11-15 15:29:01 -0800

    [Yuxin]: create shared library

---
+++ vars/genericbuild.groovy
@@ -0,0 +1,28 @@
+def call(Map config[:]) {
+    node {
+        stage('SCM') {
+            echo 'Gathering code from version control'
+            git branch: '${branch}', url: 'git@github.com:YuxinLiu076/fun-with-groovy.git', credentialsId: 'bd5c49a9-f1af-4a08-a079-fdd389446d19'
+        }
+        stage('Build') {
+            try {
+                echo 'Building....'
+                sh 'dotnet --version'
+                sh 'dotnet build ConsoleApp'
+                releasenotes(changes: "true")
+            } catch (ex) {
+                echo 'Something went wrong'
+                echo ex.toString()
+                currentBuild.result = 'FAILURE'
+            } finally {
+//             clean up
+            }
+        }
+        stage('Test') {
+            echo 'Testing....'
+        }
+        stage('Deploy') {
+            echo 'Deploying....'
+        }
+    }
+}
