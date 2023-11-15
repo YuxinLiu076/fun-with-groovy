0c71b4fe6e86ab4a218ce57b399aa90661efbcae
yuxinliu <lyx1141491623@gmail.com>
2023-11-14 16:29:08 -0800

    [Yuxin]: modify release notes

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -1,5 +1,8 @@
 import java.io.*
 import groovy.io.*
+import java.util.Calendar.*
+import java.text.SimpleDateFormat
+import hudson.model.*
 
 @NonCPS
 def call(Map config = [:]) {
@@ -8,13 +11,35 @@ def call(Map config = [:]) {
     new File(dir.path + '/releasenotes.txt').withWriter('utf-8') {
         writer ->
             // FileType.ANY: consider both directory and file
-            dir.eachFileRecurse (FileType.ANY) {
+            dir.eachFileRecurse(FileType.ANY) {
                 file ->
                     if (file.isDirectory()) {
                         writer.writeLine(file.name)
                     } else {
                         writer.writeLine('\t' + file.name + '\t' + file.length())
                     }
+            }
+
+            def date = new Date()
+            def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
+
+            echo "Date and Time is: " + sdf.format(date)
+
+            echo "Build Number is:${BUILD_NUMBER}"
+
+            def changeLogSets = currentBuild.changeSets
+            for (change in changeLogSets) {
+                def entries = change.items
+                for (entry in entries) {
+                    echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
+                    for (file in entry.affectedFiles) {
+                        echo "    ${file.editType.name} ${file.path}"
+                    }
+                }
+            }
+
+            if (config.change != 'false') {
+                echo "changes"
             }
     }
 }
