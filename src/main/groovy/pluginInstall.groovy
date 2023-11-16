33624debd47094122d64453448d3d6f7bf033e77
yuxinliu <lyx1141491623@gmail.com>
2023-11-15 16:57:29 -0800

    [Yuxin]: add plugincheck

---
+++ src/main/groovy/pluginInstall.groovy
@@ -0,0 +1,45 @@
+import jenkins.model.*
+import hudson.util.VersionNumber
+
+class PluginCheck {
+
+    String key
+    VersionNumber pluginVersion
+    def internalPlugin
+
+    PluginCheck(String artifactId, String version) {
+        key = artifactId
+        pluginVersion = new VersionNumber(version)
+        internalPlugin = new ArrayList<String>(Jenkins.getInstanceOrNull().pluginManager.plugins).find { x -> x.shortName == this.key }
+    }
+
+    String shortName() {
+        return internalPlugin.shortName
+    }
+
+    String displayName() {
+        return internalPlugin.displayName
+    }
+
+    String installedVersion() {
+        return internalPlugin.version
+    }
+
+    Boolean isInstalled() {
+        return internalPlugin != null && !internalPlugin.isOlderThan(pluginVersion)
+    }
+
+    void install() {
+        Jenkins.getInstanceOrNull().updateCenter.getPlugin(key).deploy()
+    }
+}
+
+def pluginCheck = new PluginCheck("jobConfigHistory", "2.0")
+
+if (!pluginCheck.isInstalled()) {
+    pluginCheck.install()
+} else {
+    println(pluginCheck.isInstalled() ? "Installed" : "Not Installed")
+    println(pluginCheck.shortName())
+    println(pluginCheck.installedVersion())
+}
